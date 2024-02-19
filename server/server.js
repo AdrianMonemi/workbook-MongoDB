482486c608a66ea19d74f943ed00a83699d2ae64
AdrianMonemi <adrianmonemi6@gmail.com>
2024-02-20 00:19:00 +0100

    .env added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
-import express from 'express';
+import express from "express";
 import mongoose from "mongoose";
 import router from "./src/routes/router.js";
 import cors from "cors";
+
+dotenv.config();
 
 const { MONGO_URL, PORT = 8080 } = process.env;
 
@@ -20,7 +21,13 @@ app.use("api/FAQ", router);
 const main = async () => {
   await mongoose.connect(MONGO_URL);
 
-  app.listen(PORT, () => {console.log(`App is listening on ${PORT}`);
-  console.log("Try /api/employees route right now");
-});
+  app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+    console.log("Try /api/FAQ route right now");
+  });
 };
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
